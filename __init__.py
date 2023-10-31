9416e04ac7e95d89328b278bfc3ca772791dca13
Valentina.Feijoo <valenfei@javerianacali.edu.co>
2023-10-30 19:57:52 -0500

    files updated

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from operations import add
+from operations import substract
 def game():
     score = 0
     while True:
         print('======== Menu ========'
-        '\n1. Add'
+        '\n1. Substract'
         '\n0. Exit')
         option = int(input('\nChoice an option: '))
         if option == 0:
@@ -12,7 +12,7 @@ def game():
         num_2 = input('Enter second number: ')
         answer = int(input('Enter you answer: '))
         if option == 1:
-            result = add(num_1, num_2)
+            result = substract(num_1, num_2)
             if result == answer:
                 score += 1
                 print('Correct!!')
